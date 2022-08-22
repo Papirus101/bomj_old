bb9dea54f541becb7f3a02fbfca0777b734320aa
Ilya <kirilovilya@mail.ru>
2022-08-23 00:56:12 +0300

    new commit

--- db/session.py
+++ db/session.py
@@ -3,14 +3,17 @@ import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import database_exists, create_database
 
 from db.models import ammo_db, user_db, animals_db, banda_db, cars_db, chat_random_db, donat_db, fish_db, \
     gas_station_db, guns_db, guns_war_db, houses_db, pizza_components_db, pizza_db, race_event_db, \
     shop_item_db, stuff_db, user_business_db, user_stuff_db, api_user_db, active, models_settings, items
+from db.base import Base
 
 from dotenv import load_dotenv
 
 load_dotenv('.env')
+print(os.getenv('DB_USER'))
 
 engine = create_async_engine(
     f"postgresql+asyncpg://{os.getenv('DB_USER')}:{os.getenv('DB_PASS')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}",
@@ -21,8 +24,9 @@ sync_engine = create_engine(
     f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASS')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}",
     future=True)
 
-metadata = MetaData()
-metadata.reflect(bind=sync_engine)
+if not database_exists(sync_engine.url):
+    create_database(sync_engine.url)
+    Base.metadata.create_all(bind=sync_engine)
 
 async_sessionmaker = sessionmaker(
     engine, expire_on_commit=False, class_=AsyncSession
